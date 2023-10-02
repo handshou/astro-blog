7aefd349dc865d0db6bedbc3d0ef95e074aa393f
hansel <handshou@users.noreply.github.com>
2023-10-02 08:50:48 +0800

    fix: vercel config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,29 +1,32 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-
 import cloudflare from "@astrojs/cloudflare";
 
-// https://astro.build/config
 import react from "@astrojs/react";
-
+import vercel from "@astrojs/vercel/serverless";
 const tina = ({
- directiveName = 'tina'
+  directiveName = 'tina'
 } = {}) => ({
   name: 'tina-cms',
   hooks: {
-    'astro:config:setup': ({ addClientDirective, opts }) => {
+    'astro:config:setup': ({
+      addClientDirective,
+      opts
+    }) => {
       addClientDirective({
         name: directiveName,
-        entrypoint: './client-directives/tina.mjs',
-      })
+        entrypoint: './client-directives/tina.mjs'
+      });
     }
   }
-})
+});
+
+// https://astro.build/config
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://hansel.co',
   integrations: [mdx({}), react(), tina()],
   output: "server",
-  adapter: cloudflare()
+  adapter: vercel()
 });
